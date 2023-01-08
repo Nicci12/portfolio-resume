68846a4774f77847d90638a8f5499e75f1f2a609
Your Name <ndimant@hotmail.com>
2023-01-08 17:43:28 +0200

    imprving what i do

--- src/components/Services.js
+++ src/components/Services.js
@@ -9,8 +9,7 @@ const Services = () => {
     <section
       id="services"
       data-nav-tooltip="Services"
-      className="pp-section pp-scrollable section"
-    >
+      className="pp-section pp-scrollable section">
       <div className="container">
         <div className="title">
           <h3>What I do?</h3>
@@ -22,7 +21,8 @@ const Services = () => {
               <div className="feature-content media-body">
                 <h5>Web Design</h5>
                 <p>
-                As a Web designer I am responsible for desigining different websites that are displayed on the internet.
+                  As a Web designer I am responsible for desigining different
+                  websites that are displayed on the internet.
                 </p>
               </div>
             </div>
@@ -33,7 +33,8 @@ const Services = () => {
               <div className="feature-content media-body">
                 <h5>Web Development</h5>
                 <p>
-               As a web devekloper I am responsible for creating the structure, layout, and functionality of a website.
+                  As a web devekloper I am responsible for creating the
+                  structure, layout, and functionality of a website.
                 </p>
               </div>
             </div>
@@ -44,7 +45,12 @@ const Services = () => {
               <div className="feature-content media-body">
                 <h5>SEO Marketing</h5>
                 <p>
-                SEO, or search engine optimization, is the practice of improving the ranking of a website on search engines like Google. I achieve this through a variety of techniques, such as optimizing the content and structure of a website, building high-quality backlinks, and making the website responsive and mobile-friendly.
+                  SEO, or search engine optimization, is the practice of
+                  improving the ranking of a website on search engines like
+                  Google. I achieve this through a variety of techniques, such
+                  as optimizing the content and structure of a website, building
+                  high-quality backlinks, and making the website responsive and
+                  mobile-friendly.
                 </p>
               </div>
             </div>
@@ -53,10 +59,14 @@ const Services = () => {
             <div className="feature-box-01 media">
               <i className="icon theme-bg icon-bugz" />
               <div className="feature-content media-body">
-                <h5>Web Design</h5>
+                <h5>Unit Testing</h5>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  The goal of unit testing is to validate that each unit of the
+                  application is working as intended and meets the specified
+                  requirements. Unit tests are typically automated, and they are
+                  run every time the code is changed to ensure that new changes
+                  have not introduced any regressions or broken any existing
+                  functionality.
                 </p>
               </div>
             </div>
@@ -65,10 +75,11 @@ const Services = () => {
             <div className="feature-box-01 media">
               <i className="icon theme-bg icon-desktop" />
               <div className="feature-content media-body">
-                <h5>Web Development</h5>
+                <h5>Debbuging</h5>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  I identify and remove errors from code as it allows me to
+                  locate and fix problems in my code, which can help them to
+                  improve the quality of my code and make it more reliable.
                 </p>
               </div>
             </div>
@@ -77,10 +88,9 @@ const Services = () => {
             <div className="feature-box-01 media">
               <i className="icon theme-bg icon-desktop" />
               <div className="feature-content media-body">
-                <h5>Web Development</h5>
+                <h5>AWS And GCS</h5>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                 I am suffiecient in both Amazon Web Sevices and Google Cloud Services
                 </p>
               </div>
             </div>
